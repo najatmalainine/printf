02ffe8892b623f062d98f82276337d2e32f51f5f
Najat <craazylolla@gmail.com>
2023-07-23 12:05:44 -0700

    m

--- _printf.c
+++ _printf.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int val_precentage(const char *format, int *i);
-
 /**
  * _printf - prints a formatted string to stdout, similar to printf.
  * @format: the format of the string to be printed.
@@ -14,69 +12,27 @@ int val_precentage(const char *format, int *i);
  */
 int _printf(const char *format, ...)
 {
-	va_list args, args_flags, args_width;
-	flags_t flags = {0};
-	width_t width = {0};
-	int (*print_fn)(va_list);
-	int i = 0, prd = 0;
+	va_list args;
+	int (*pfn)(va_list);
+	int i = 0, printed = 0;
 
 	if (!format)
 		return (-1);
+
 	va_start(args, format);
-	(va_copy(args_flags, args), va_copy(args_width, args));
+
 	for (; format && format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
-			if (!val_precentage(format, &i))
-				return (-1);
-			/* get conversion_index by getting [flags, width..] first */
-			print_fn = get_conversion(format, &i, args, &flags, &width);
-
-			par_flags(&flags, args_flags, print_fn, &prd);
-			par_width(&width, args_width, print_fn, &prd);
-			par_length(&flags, args, print_fn, &prd);
-
-			/* check length outside this func*/
-			if (flags.h || flags.l)
-				continue;
-			/* for valid conversion call print_func, otherwise print as is*/
-			prd += print_fn
-						   ? print_fn(args)
-						   : _putchar('%') + _putchar(format[i]);
+			/*  */
+			pfn = get_print(&format[++i]);
+			printed += pfn(args);
 		}
 		else
-			prd += _putchar(format[i]);
+			printed += _putchar(format[i]);
 	}
+
 	va_end(args);
-	(va_end(args_flags), va_end(args_width));
-	return (prd);
-}
-
-/**
- * val_precentage - valid percentages
- * @format: The string to be evaluated
- * @i: Pointer to the index of the character being evaluated
- * Return: 1 if valid specifer, 0 otherwise
- */
-int val_precentage(const char *format, int *i)
-{
-	int k;
-
-	if (format[++(*i)] == '\0')
-		return (0);
-	k = *i;
-
-	/* check if no caractere after '%', return false */
-	for (; format[k] == ' '; k++)
-	{
-		if (format[*i + 1] == '\0')
-			return (0);
-		else if (format[*i + 1] == ' ')
-			(*i)++;
-		else
-			break;
-	}
-	/* ignore extra val_precentage between '%' and 'specefier' */
-	return (1);
+	return (printed);
 }
