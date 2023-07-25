154fce648268568e2d91c73bf27b4f22528abdf4
Najat <craazylolla@gmail.com>
2023-07-25 06:50:37 -0700

    description_fix

--- get_conv.c
+++ get_conv.c
@@ -4,9 +4,9 @@
  * convert - converts number and base into string
  * @num: input number
  * @base: input base
+ * @low: flag if hexa values need to be lowercase
  * Return: result string
  */
-
 char *convert(unsigned long int num, int base, int low)
 {
 	static char *rep;
