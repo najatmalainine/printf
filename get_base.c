f29f1173d400906f54d1c13e2d46ede5d7026cc0
Najat <craazylolla@gmail.com>
2023-07-25 10:19:06 -0700

    adding print hex uppercase

--- get_base.c
+++ get_base.c
@@ -19,3 +19,21 @@ int printHex(va_list l, flags_t *f)
 	count += _puts(str);
 	return (count);
 }
+/**
+ * printHex_b - prints a number in hexadecimal base in uppercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Return: the number of char printed
+ */
+int printHex_b(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 0);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0X");
+	count += _puts(str);
+	return (count);
+}
