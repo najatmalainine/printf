d924b3b01282403fda0ae4052b2c1b8827c8c020
Najat <craazylolla@gmail.com>
2023-07-25 06:35:51 -0700

    first

---
+++ get_base.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * printHex - prints a number in hexadecimal base,
+ * in lowercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: the number of char printed
+ */
+int printHex(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 1);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0x");
+	count += _puts(str);
+	return (count);
+}
