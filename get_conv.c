e2756c25cd79f2799cb8054e10bc3245e03e1da7
Najat <craazylolla@gmail.com>
2023-07-25 06:48:06 -0700

    descript

--- get_conv.c
+++ get_conv.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * convert - converts number and base into string
+ * @num: input number
+ * @base: input base
+ * Return: result string
+ */
 
 char *convert(unsigned long int num, int base, int low)
 {
