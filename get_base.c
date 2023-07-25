c94b4b7f5320286d86638765538c9c967a127961
Najat <craazylolla@gmail.com>
2023-07-25 13:26:05 -0700

    adding printal octal

--- get_base.c
+++ get_base.c
@@ -55,3 +55,25 @@ int printBinary(va_list l, flags_t *f)
 	(void)f;
 	return (_puts(str));
 }
+
+/**
+ * printOctal - prints a number in base 8
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int printOctal(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 8, 0);
+	int count = 0;
+
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _putchar('0');
+	count += _puts(str);
+	return (count);
+}
