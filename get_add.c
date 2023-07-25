085117728eba3dd4f12fb6ec1bbcf51d67f236b9
Najat <craazylolla@gmail.com>
2023-07-25 06:04:57 -0700

    first

---
+++ get_add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * printAdd - prints address of input in hexa format
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int printAdd(va_list l, flags_t *f)
+{
+	char *str;
+	unsigned long int p;
+
+	register int count;
+
+	p = va_arg(l, unsigned long int);
+	count = 0;
+	
+	(void)f;
+
+	if (!p)
+		return (_puts("(nil)"));
+	str = convert(p, 16, 1);
+	count += _puts(str);
+	return (count);
+}
